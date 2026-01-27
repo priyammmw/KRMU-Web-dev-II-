7de15e002b4efd6b34d6dae370332ea7e724fbeb
Priyam Sharma <priyamsharmaps47@gmail.com>
2026-01-27 19:00:29 +0530

    Add files via upload

---
+++ Day4/javaday4.js
@@ -0,0 +1,10 @@
+let user={
+    FullName:"Priyam Sharma",
+    address:"Delhi, India",
+    mobileno:"9876543210",
+    favcolor:["Red","Blue","Green"], 
+}
+console.log(user)
+ 
+//we can acess the individual value by targeting key with variable name
+// console.log(user.fullname,user.address,user.numner,user.favcolour[1]);
