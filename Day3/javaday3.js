e5e9b70778f489a81835ec34843d331da3e0efde
Priyam Sharma <priyamsharmaps47@gmail.com>
2026-01-27 18:59:49 +0530

    Add files via upload

---
+++ Day3/javaday3.js
@@ -0,0 +1,44 @@
+// let sentence="i am a comma seperated sentence";
+// let words=sentence.split(" ");
+// console.log(words)
+
+// let sentence2="i am a comma seperated sentence";
+// let words2=sentence2.split("a");
+// console.log(words2)
+
+//Q1.find out the occurance of "r" in the given string
+// let str="tu meri mai tera ,mai teri tu mera"
+// let count=str.split("r").length-1;
+// console.log("the occurance of 'r' in the given string is:"+count);
+
+// let str="tu meri mai tera ,mai teri tu mera"
+// let res=str.split(" r");
+// console.log(res);
+
+//types of functions
+//1.regular function
+// function sum(){
+//     console.log("demo of regular function");
+// }
+// Demo()
+
+// function sample(a,b){
+//     return a+b;
+// }    
+// console.log(sample(5,10))
+
+//Arrow function//
+// advantage: doesn't need return and function keyword and it will perform asynchronously.
+//const test=(c,d)=> c+d;
+//console.log(test(4,6))   
+
+// const test = (c,d)=>{
+//     return c+d
+// };
+// console.log(test(4,6))
+
+// let arr = [1,2,3,4,5]
+// let res = arr.map(function(x)){
+//     return x+2
+// }
+// console.log
