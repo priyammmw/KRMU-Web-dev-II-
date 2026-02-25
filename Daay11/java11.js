1a4e78eb9b0465c96579d64e443d9e314af4634a
Priyam Sharma <priyamsharmaps47@gmail.com>
2026-02-25 23:36:54 +0530

    Update java11.js

--- Daay11/java11.js
+++ Daay11/java11.js
@@ -19,36 +19,4 @@
 // }
 // );
 // console.log("Finished");
-// day12---------------------------------------------------
-
-function login(email,pass,cb){
-    setTimeout(() => {
-        cb({userid:email,isloggedIN:true,message:"login successful"});
-    }, 3000);
-}
-// login("user@example.com", "password", (userdetails) => {
-//     console.log(userdetails);
-// });
-function getvideolist(cb){
-    setTimeout(() => {
-    cb(["video1", "video2", "video3"]);
-    }, 4000);
-}
-login("user@example.com", "password", (userdetails) => {
-    console.log(userdetails);
-    getvideolist((videos) => {
-    console.log(videos);
-})});
-function getvideodetails(user,cb){
-    setTimeout(() => {
-        cb({title:"VideoColorSpace",duration:"2 mins"});
-        }, 4000);
-}
-login("user@example.com", "password", (userdetails) => {
-    console.log(userdetails);
-    getvideolist(userdetails.userid, (videodetails) => {
-    console.log(videodetails);
-    getvideodetails(userdetails.userid, (videodetails) => {
-        console.log(videodetails);
-    }))});
  
